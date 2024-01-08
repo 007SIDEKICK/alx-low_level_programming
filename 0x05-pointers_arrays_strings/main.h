5b7817e385c309d0dbc931fe663606302681fde1
007SIDEKICK <ledi13317@gmail.com>
2024-01-08 11:19:47 -0800

    emset

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+Char *_memset(char *s, char b, unsigned int n);
+Int _putchar(char c)
+Char *_memcpy(char *dest, char *src, unsigned int n);
+Char *_strchr(char *s, char c);
+Unsigned int _strspn(char *s, char *accept);
+Char *strpbrk(char *s, char *accept);
+Char *strstr(char *haystack, *char *needle);
+Void print_chessboard(char (*a)[8]);
+Void print_diagsums(int *a, int size) ;
+Void set_string(char **s, char *to) ;
+
+#endif
+
